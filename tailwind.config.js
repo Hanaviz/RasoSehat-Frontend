865d0c202296d1101e62dba67da18306be83ed00
Hanaviz <potter1320092004@gmail.com>
2025-12-12 02:06:38 +0700

    fix: tailwind config to ESM; restore tailwind directives

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx,html}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui')
-  ],
-  daisyui: {
-    themes: [
-      'light',
-    ],
-  },
-}
+import daisyui from 'daisyui';
+
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,html}'],
+  theme: { extend: {} },
+  plugins: [daisyui],
+  daisyui: { themes: ['light'] },
+};
