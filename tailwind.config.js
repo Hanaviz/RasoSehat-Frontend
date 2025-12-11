1a228b081cc8fd0dd6929e19b018d8e7ee3464e6
Hanaviz <potter1320092004@gmail.com>
2025-12-12 02:09:21 +0700

    fix tampilan rusak

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui';
-
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,html}'],
-  theme: { extend: {} },
-  plugins: [daisyui],
-  daisyui: { themes: ['light'] },
-};
